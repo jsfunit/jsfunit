9f8222fd7be66b39734bdf40189049eb10148cfd
Jason Fager <l.jinok.l@gmail.com>
2008-07-21 20:12:09 +0000

    added el class resolution information for jsf's implicit objects.
    git-svn-id: https://svn.jboss.org/repos/jsfunit/trunk@330 992343fe-802f-0410-85da-d31fbad5f0a6

---
+++ jboss-jsfunit-analysis/src/main/java/org/jboss/jsfunit/analysis/el/ImplicitObjects.java
@@ -0,0 +1,44 @@
+package org.jboss.jsfunit.analysis.el;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class ImplicitObjects
+{
+	private static final Map<String, Class<?>> implicitObjectMap
+		= new HashMap<String, Class<?>>() {
+            private static final long serialVersionUID = 20080721L;
+		{
+			put("application", ServletContext.class);
+			put("cookie", Map.class);
+			put("facesContext", FacesContext.class);
+			put("header", Map.class);
+			put("headerValues", Map.class);
+			put("param", Map.class);
+			put("paramValues", Map.class);
+			put("request", ServletRequest.class);
+			put("session", HttpSession.class);
+			put("requestScope", Map.class);
+			put("sessionScope", Map.class);
+			put("applicationScope", Map.class);
+			put("initParam", Map.class);
+			put("view", UIViewRoot.class);
+	}};
+
+
+	/**
+	 * Returns a map that is safe for modification.
+	 *
+	 * @return
+	 */
+	public static Map<String, Class<?>> getMap() {
+		return new HashMap<String, Class<?>>(implicitObjectMap);
+	}
+
+}
