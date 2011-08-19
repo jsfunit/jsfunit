9b23fa829b1c5bf7f7e6f01c6aec6853f89a828c
joserodolfofreitas <joserodolfo.freitas@gawab.com>
2011-08-19 11:20:20 -0300

    added webArchive.delete(WEB_XML) so the new web.xml could 'replace' the old one.

--- jboss-jsfunit-arquillian/src/main/java/org/jboss/jsfunit/arquillian/client/JSFUnitProtocolArchiveProcessor.java
+++ jboss-jsfunit-arquillian/src/main/java/org/jboss/jsfunit/arquillian/client/JSFUnitProtocolArchiveProcessor.java
@@ -17,8 +17,8 @@
  */
 package org.jboss.jsfunit.arquillian.client;
 
-import org.jboss.arquillian.spi.TestDeployment;
-import org.jboss.arquillian.spi.client.deployment.ProtocolArchiveProcessor;
+import org.jboss.arquillian.container.test.spi.TestDeployment;
+import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
 import org.jboss.jsfunit.arquillian.container.JSFUnitCleanupTestTreadFilter;
 import org.jboss.jsfunit.framework.JSFUnitFilter;
 import org.jboss.shrinkwrap.api.Archive;
@@ -31,63 +31,51 @@ import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 import org.jboss.shrinkwrap.descriptor.api.spec.servlet.web.WebAppDescriptor;
 
 /**
- * Extension that will add JSFUnit required filters for Servlet 2.5 based applications.  
- *
+ * Extension that will add JSFUnit required filters for Servlet 2.5 based applications.
+ * 
  * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
  * @version $Revision: $
  */
-public class JSFUnitProtocolArchiveProcessor implements ProtocolArchiveProcessor
-{
-   private static final ArchivePath WEB_XML = ArchivePaths.create("WEB-INF/web.xml");
-   private static final Double WEB_XML_VERSION = 2.5;
-   
-   /**
-    * If this is a Servlet 2.5 Archive, append the JSFUnit filter to the web.xml.
-    */
-   @Override
-   public void process(TestDeployment testDeployment, Archive<?> protocolArchive)
-   {
-      if(WebArchive.class.isInstance(protocolArchive))
-      {
-         WebArchive webArchive = WebArchive.class.cast(protocolArchive);
-         
-         if(webArchive.contains(WEB_XML))
-         {
-            WebAppDescriptor descriptor = loadDescriptor(webArchive.get(WEB_XML));
-            if(shouldAddJSFUnitFilters(descriptor))
-            {
-               addJSFUnitFilters(descriptor);
-               webArchive.add(new StringAsset(descriptor.exportAsString()), WEB_XML);
-            }
-         }
-      }
-   }
-   
-   private boolean shouldAddJSFUnitFilters(WebAppDescriptor descriptor)
-   {
-      try
-      {
-         Double definedVersion = Double.parseDouble(descriptor.getVersion());
-         return definedVersion <= WEB_XML_VERSION;
-      }
-      catch (NumberFormatException e) 
-      {
-         throw new RuntimeException(
-               "Could not parse the web.xml version number to determine if it is less or equal to 2.5, " +
-         		"this so we can merge in the JSFUnit support", e);
-      }
-   }
+public class JSFUnitProtocolArchiveProcessor implements ProtocolArchiveProcessor {
+    private static final ArchivePath WEB_XML = ArchivePaths.create("WEB-INF/web.xml");
+    private static final Double WEB_XML_VERSION = 2.5;
 
-   private WebAppDescriptor loadDescriptor(Node node)
-   {
-      return Descriptors.importAs(WebAppDescriptor.class).from(
-            node.getAsset().openStream());
-   }
-   
-   private WebAppDescriptor addJSFUnitFilters(WebAppDescriptor descriptor)
-   {
-      return descriptor
-               .filter(JSFUnitCleanupTestTreadFilter.class, "/ArquillianServletRunner")
-               .filter(JSFUnitFilter.class, "/ArquillianServletRunner");
-   }
+    /**
+     * If this is a Servlet 2.5 Archive, append the JSFUnit filter to the web.xml.
+     */
+    @Override
+    public void process(TestDeployment testDeployment, Archive<?> protocolArchive) {
+        if (WebArchive.class.isInstance(protocolArchive)) {
+            WebArchive webArchive = WebArchive.class.cast(protocolArchive);
+
+            if (webArchive.contains(WEB_XML)) {
+                WebAppDescriptor descriptor = loadDescriptor(webArchive.get(WEB_XML));
+                if (shouldAddJSFUnitFilters(descriptor)) {
+                    addJSFUnitFilters(descriptor);
+                    webArchive.delete(WEB_XML);
+                    webArchive.add(new StringAsset(descriptor.exportAsString()), WEB_XML);
+                }
+            }
+        }
+    }
+
+    private boolean shouldAddJSFUnitFilters(WebAppDescriptor descriptor) {
+        try {
+            Double definedVersion = Double.parseDouble(descriptor.getVersion());
+            return definedVersion <= WEB_XML_VERSION;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(
+                    "Could not parse the web.xml version number to determine if it is less or equal to 2.5, "
+                            + "this so we can merge in the JSFUnit support", e);
+        }
+    }
+
+    private WebAppDescriptor loadDescriptor(Node node) {
+        return Descriptors.importAs(WebAppDescriptor.class).from(node.getAsset().openStream());
+    }
+
+    private WebAppDescriptor addJSFUnitFilters(WebAppDescriptor descriptor) {
+        return descriptor.filter(JSFUnitCleanupTestTreadFilter.class, "/ArquillianServletRunner").filter(JSFUnitFilter.class,
+                "/ArquillianServletRunner");
+    }
 }
